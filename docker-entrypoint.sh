4666c5972bb9abba7f10899165451d46551bbfe7
Bryan Larson <b32147@gmail.com>
2017-09-09 19:33:53 -0400

    Fixed entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Add user.
 adduser -s /sbin/nologin -h /home/samba -H -D $USERNAME
-printf '$PASSWORD\n$PASSWORD\n' | smbpasswd -sa $USERNAME
+printf "%s\n%s\n" "$PASSWORD" "$PASSWORD" | smbpasswd -sa $USERNAME
 
 # Run samba.
 smbd --foreground --log-stdout
